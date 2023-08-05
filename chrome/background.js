073ee24c04c457160a11b9540ef63470597b2c61
Mykhailo Zachepylo <mihailzachepilo@gmail.com>
2023-08-05 16:50:06 +0300

    Request less permissions, do not add content script to each youtube tab

---
+++ chrome/background.js
@@ -0,0 +1,66 @@
+function executeYouTubeCommand(action) {
+  const getAddVideoParams = (videoId) => ({
+    clickTrackingParams: "",
+    commandMetadata: { webCommandMetadata: { sendPost: true, apiUrl: "/youtubei/v1/browse/edit_playlist" } },
+    playlistEditEndpoint: { playlistId: "WL", actions: [{ addedVideoId: videoId, action: "ACTION_ADD_VIDEO" }] }
+  });
+  
+  const getRemoveVideoParams = (videoId) => ({
+    clickTrackingParams: "",
+    commandMetadata: { webCommandMetadata: { sendPost: true, apiUrl: "/youtubei/v1/browse/edit_playlist" } },
+    playlistEditEndpoint: { playlistId: "WL", actions: [{ action: "ACTION_REMOVE_VIDEO_BY_VIDEO_ID", removedVideoId: videoId }] }
+  });
+
+  const sendActionToNativeYouTubeHandler = (getParams) => {
+    const videoId = new URL(window.location.href).searchParams.get("v");
+    const appElement = document.querySelector("ytd-app");
+
+    if (!videoId || !appElement) {
+      return;
+    }
+  
+    const event = new window.CustomEvent('yt-action', {
+      detail: {
+        actionName: 'yt-service-request',
+        returnValue: [],
+        args: [{ data: {} }, getParams(videoId)],
+        optionalAction: false,
+      }
+    });
+  
+    appElement.dispatchEvent(event);
+  };
+
+  try {
+    if (action === "add-to-watch-later") {
+      sendActionToNativeYouTubeHandler(getAddVideoParams);
+    }
+    if (action === "remove-from-watch-later") {
+      sendActionToNativeYouTubeHandler(getRemoveVideoParams);
+    }
+  } catch (error) {
+    console.warn("Error while sending message to native YouTube handler", error);
+  }
+}
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "add-to-watch-later" || command === "remove-from-watch-later") {
+    const [activeYouTubeTab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+      url: 'https://www.youtube.com/*',
+    });
+
+    if (!activeYouTubeTab) {
+      return;
+    }
+
+    await chrome.scripting.executeScript({
+      target: {
+        tabId: activeYouTubeTab.id,
+      },
+      func: executeYouTubeCommand,
+      args: [command],
+    });
+  }
+});
--- src/background.js
+++
@@ -1,7 +0,0 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "add-to-watch-later" || command === "remove-from-watch-later") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: command });
-    });
-  }
-});
